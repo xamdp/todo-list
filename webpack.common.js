cd3ace3cde81b0599a8220438eb61ae23db7c6a1
xamdp <64017084+xamdp@users.noreply.github.com>
2026-09-07 18:16:48 +0900

    build: add svg-inline-loader

--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,23 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
 	entry: {
-		app: './src/index.js',
+		app: "./src/index.js",
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: "./src/template.html",
-			title: 'Production',
+			title: "Production",
 		}),
 	],
 	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist'),
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
 		clean: true,
 	},
 	module: {
@@ -31,9 +31,13 @@ export default {
 				use: ["html-loader"],
 			},
 			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				test: /\.(png|jpg|jpeg|gif)$/i,
 				type: "asset/resource",
-			}
+			},
+			{
+				test: /\.svg$/,
+				use: "svg-inline-loader",
+			},
 		],
-	}
+	},
 };
